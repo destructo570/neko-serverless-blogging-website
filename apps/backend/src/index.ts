54d595bd3f47fbc9c46dc4f14c7735551c67aa7a
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-07-20 13:01:14 +0530

    refac: refactor code

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import blogRoutes from "./routes/blog";
 import authRoutes from "./routes/auth";
 
+//All API's start with this base path
 const app = new Hono().basePath("/api/v1");
 
 app.route("/blog", blogRoutes);
